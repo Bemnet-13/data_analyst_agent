17cb4495a41256c6f61da8ed11a55949b590de97
Bemnet Aschalew <bemnetaschalewwodaj@gmail.com>
2025-07-09 16:14:41 +0300

    first commit

---
+++ data_analyst_agent/agent.py
@@ -0,0 +1,22 @@
+"""data_analyst_agent for finding information using google search"""
+
+from google.adk import Agent
+from google.adk.tools.agent_tool import AgentTool
+from data_analyst_agent.tools.financial_data import financial_data
+from data_analyst_agent.tools.stock_price import get_stock_market_data
+from data_analyst_agent.subagents.market_news_agent.agent import market_news_agent
+
+from . import prompt
+
+MODEL = "gemini-2.5-flash"
+market_news_analyzer = AgentTool(agent=market_news_agent)
+
+data_analyst_agent = Agent(
+    model=MODEL,
+    name="data_analyst_agent",
+    instruction=prompt.DATA_ANALYST_PROMPT,
+    output_key="market_data_analysis_output",
+    tools=[get_stock_market_data, financial_data, market_news_analyzer],
+)
+
+root_agent = data_analyst_agent
---
+++ data_analyst_agent/subagents/market_news_agent/agent.py
@@ -0,0 +1,16 @@
+"""News sentiment analysis agent for market news"""
+
+from google.adk import Agent
+
+from . import prompt
+from data_analyst_agent.tools.news_sentiment import market_news_sentiment
+
+MODEL="gemini-2.5-pro"
+
+market_news_agent = Agent(
+    model=MODEL,
+    name="market_news_agent",
+    instruction=prompt.NEWS_AGENT_PROMPT,
+    tools=[market_news_sentiment],
+    output_key="news_sentiment_analysis_output",
+)
